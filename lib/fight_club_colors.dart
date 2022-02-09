e6f666796baabd14100f5d588510f95669bfea39
Alexandr Timoschuk <app.gelios@gmail.com>
2022-02-09 14:37:37 +0300

    update, 6 tasks renew

---
+++ lib/fight_club_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FightClubColors {
+
+  FightClubColors._();
+
+  static Color background = const Color.fromRGBO(213, 222, 240, 1);
+  static Color greyButton = const Color.fromRGBO(0, 0, 0, 0.38);
+  static Color blueButton = const Color.fromRGBO(28, 121, 206, 1);
+  static Color greenButton = const Color.fromRGBO(77, 200, 57, 1);
+  static Color blackButton = const Color.fromRGBO(0, 0, 0, 0.87);
+  static Color darkGreyText = const Color.fromRGBO(22, 22, 22, 1);
+  static Color whiteText = const Color.fromRGBO(255, 255, 255, 0.87);
+  static Color centerBox = const Color.fromRGBO(197, 209, 234, 1);
+  static Color youSide = const Color.fromRGBO(255, 255, 255, 1);
+
+}
